6ce79083de5fa7ac871a3ead598fad3b8de003ba
DevHedronYT <altair.d.ghost@gmail.com>
2021-06-15 11:50:57 +0200

    First Contributor: NrdyBhu

--- run-linux.sh
+++ run-linux.sh
@@ -1,40 +1,21 @@
+<<<<<<< HEAD
 sudo apt-get install -y libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
 
+=======
+sudo apt-get install -y libxrandr-dev libxinerama-dev libxcursor-devlibxi-dev
+>>>>>>> 191f321c3e2357acdaab716d15a758c2d4f24fad
 pwd
 
 appFile=./app
-libGLFW=./Libs/libglfw3.a
-Lib=./Libs
 
-if test -f "$Libs"; then
-    echo
-else
-    mkdir Libs
-fi
-
-if test -f "$libGLFW"; then
-    echo
-else 
-    cd Libs
-    rm -rf *
-    git clone https://github.com/glfw/glfw.git GLFW
-    cd GLFW && mkdir build && cd build
-    cmake .. && make
-    cd src
-    ls -al
-    mv libglfw3.a ../../../
-    cd ../../..
-    rm -rf GLFW
-    cd ..
-fi
-
+sh ./install-glfw-deps.sh
 
 if test -f "$appFile"; then
     rm app
-
 else 
     echo Compiling...
 fi
 
 export DYLD_LIBRARY_PATH=Libs
+cd Test
 make linux 
