06189f532dde04bb91ee48be914671061b2c4a09
DevHedronYT <altair.d.ghost@gmail.com>
2021-06-15 10:34:16 +0200

    Changed Makefile Name + Bunch of Other Stuff

--- run-linux.sh
+++ run-linux.sh
@@ -1,7 +1,4 @@
-sudo apt-get install -y libxrandr-dev
-sudo apt-get install -y libxinerama-dev
-sudo apt-get install -y libxcursor-dev
-sudo apt-get install -y libxi-dev
+sudo apt-get install -y libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
 
 pwd
 
