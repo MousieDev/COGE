27417ddc3206102c40e99aaee737c54c2a1e68f4
DevHedron <59399239+DevHedronYT@users.noreply.github.com>
2021-06-17 22:09:15 +0200

    Added X11 Headers

--- run-linux.sh
+++ run-linux.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev xorg-dev cmake
+sudo apt-get install -y libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev xorg-dev cmake libx11-dev
 
 appFile=./app
 
